281e3c89f229bf702e2686ab28ca6a9cc136e0ee
ixelizm <felekburak8@gmail.com>
2022-05-10 18:18:58 +0300

    modules ayarlandı

---
+++ Ultro/__main__.py
@@ -0,0 +1,9 @@
+# (C) 2021 Jayant Kageri
+
+from pyrogram import idle, Client, filters
+from Ultro import app, LOGGER
+from Ultro.modules import *
+import os
+
+print("Pyrogram User Client is Starting... \nPowered By @TheCodents\n(C) 2021 Jayant Kageri" end="\n\n")
+app.run()
